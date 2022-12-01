c018fdd57e44c481e9cf8c9b2561add8c1ca51e7
Inuri Lavanya <lilh99lilh@gmail.com>
2022-12-01 08:34:59 +0530

    Remove header

--- views/profile.php
+++ views/profile.php
@@ -1,186 +1,151 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Browser meta tags -->
-    <meta charset="UTF-8" />
-    <meta content="Lambda Learning Management System" name="description" />
-    <meta
-        content="Lambda, Learning Management System, LMS, Lambda Learning, University"
-        name="keywords"
-    />
-    <meta content="Fusion 0-1" name="author">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-
-    <!-- Open Graph meta tags for social media sharing -->
-    <meta content="Lambda - Fusion 0-1" property="og:title" />
-    <meta
-        content="Lambda Learning Management System"
-        property="og:description"
-    />
-
-
-    <!-- Fonts -->
-    <!-- CSS -->
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/profile.css">
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="/images/logo.png">
-
-    <!-- Icons: FontAwesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-    <title>Lambda - Learn </title>
-</head>
-<body>
-    <div class="border main-container v-center flex-gap">
-        <div class="flex h-center v-center">
-            <div>
-                <h2 class="text-center">Inuri Lavanya</h2>
-                <h3 class="text-center text-normal line-height">Student</h3>
-            </div>
-            <div>
-<!--                <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
-            </div>
-
+<link rel="stylesheet" href="css/profile.css">
+<div class="border main-container v-center flex-gap">
+    <div class="flex h-center v-center">
+        <div>
+            <h2 class="text-center">Inuri Lavanya</h2>
+            <h3 class="text-center text-normal line-height">Student</h3>
         </div>
-
-        <div class="flex h-center flex-gap">
-            <div class="border main-container flex-gap">
-                <!-- User details -->
-
-                <h3 class="text-center">User Details</h3>
-
-                <img src="images/profile.png" alt="profile" class="profile_img profile_img_center"><br>
-
-                <div class="margin-top">
-                    <label class="margin-top">Index Number</label> <br>
-                    <div class="flex">
-                        <input type="text" value="20000715" class="input text-right width-full"><br>
-                    </div>
-                </div>
-
-                <div class="margin-top">
-                    <label class="margin-top">Registration Number</label><br>
-                    <div class="flex">
-                        <input type="text" value="20000715" class="input text-right width-full"><br>
-                    </div>
-                </div>
-
-                <div class="margin-top">
-                    <label class="margin-top">Email</label><br>
-                    <div class="flex">
-                        <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
-<!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
-                    </div>
-                </div>
-
-                <div class="margin-top">
-                    <label class="margin-top">Contact Number</label><br>
-                    <div class="flex">
-                        <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
-<!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
-                    </div>
-                </div>
-
-                <div class="margin-top">
-                    <label class="margin-top">Personal Email</label><br>
-                    <div class="flex">
-                        <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
-<!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
-                    </div>
-                </div>
-                <div class="flex margin-top">
-                    <button class="edit-btn edit-btn-text width-full">Change Password</button>
-                    <button id="edit" class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>
-                </div>
-            </div>
-
-            <div id="modal" class="modal" >
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <form>
-                        <div class="margin-top">
-                            <label>Existing Password</label><br>
-                            <input type="password" name="password" class="input text-right width-full">
-                        </div>
-                        <div class="margin-top">
-                            <label>New Password</label><br>
-                            <input type="password" name="password" class="input text-right width-full">
-                        </div>
-                        <div class="margin-top">
-                            <label>Confirm Password</label><br>
-                            <input type="password" name="password" class="input text-right width-full">
-                        </div>
-                        <button class="confirm-btn margin-top flex h-center v-center">Confirm</button>
-                    </form>
-                </div>
-            </div>
-
-            <div class="flex-wrap">
-                <div class="border main-container flex-gap">
-                    <!--Login Activity-->
-                    <h3>Login Activity</h3>
-                    <h4 class="text-normal text-center">Last Login Date : Monday, October 31, 2022  |  08:00</h4>
-                    <h4 class="text-normal text-center">Last Logout Date : Monday, October 31, 2022  |  08:00</h4>
-                </div>
-                <div class="border main-container flex-gap">
-                    <!--Registered Courses-->
-                    <h3>Registered Courses</h3>
-                    <table>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Data Structures and Algorithms III</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Rapid Application Development</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Mathematical Methods</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Software Engineering III</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Computer Networks</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Functional Programming</td>
-                        </tr>
-                        <tr>
-                            <td>SCS2201</td>
-                            <td>Programming Language Concepts</td>
-                        </tr>
-                    </table>
-
-                </div>
-            </div>
-
+        <div>
+            <!--                <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
         </div>
 
     </div>
 
-    <script>
-        var modal = document.getElementById("modal");
-        var btn = document.getElementById("edit");
-        var span = document.getElementsByClassName("close");
+    <div class="flex h-center flex-gap">
+        <div class="border main-container flex-gap">
+            <!-- User details -->
 
-        btn.onclick = function (){
-            modal.style.display = "block";
-        }
-        span.onclick = function (){
+            <h3 class="text-center">User Details</h3>
+
+            <img src="images/profile.png" alt="profile" class="profile_img profile_img_center"><br>
+
+            <div class="margin-top">
+                <label class="margin-top">Index Number</label> <br>
+                <div class="flex">
+                    <input type="text" value="20000715" class="input text-right width-full"><br>
+                </div>
+            </div>
+
+            <div class="margin-top">
+                <label class="margin-top">Registration Number</label><br>
+                <div class="flex">
+                    <input type="text" value="20000715" class="input text-right width-full"><br>
+                </div>
+            </div>
+
+            <div class="margin-top">
+                <label class="margin-top">Email</label><br>
+                <div class="flex">
+                    <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
+                    <!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
+                </div>
+            </div>
+
+            <div class="margin-top">
+                <label class="margin-top">Contact Number</label><br>
+                <div class="flex">
+                    <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
+                    <!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
+                </div>
+            </div>
+
+            <div class="margin-top">
+                <label class="margin-top">Personal Email</label><br>
+                <div class="flex">
+                    <input type="text" value="2020cs0071@gmail.com" class="input text-right width-full">
+                    <!--                        <button class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>-->
+                </div>
+            </div>
+            <div class="flex margin-top">
+                <button class="edit-btn edit-btn-text width-full">Change Password</button>
+                <button id="edit" class="edit-btn edit-btn-icon"><i class="fa-solid fa-pen"></i></button><br>
+            </div>
+        </div>
+
+        <div id="modal" class="modal" >
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <form>
+                    <div class="margin-top">
+                        <label>Existing Password</label><br>
+                        <input type="password" name="password" class="input text-right width-full">
+                    </div>
+                    <div class="margin-top">
+                        <label>New Password</label><br>
+                        <input type="password" name="password" class="input text-right width-full">
+                    </div>
+                    <div class="margin-top">
+                        <label>Confirm Password</label><br>
+                        <input type="password" name="password" class="input text-right width-full">
+                    </div>
+                    <button class="confirm-btn margin-top flex h-center v-center">Confirm</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="flex-wrap">
+            <div class="border main-container flex-gap">
+                <!--Login Activity-->
+                <h3>Login Activity</h3>
+                <h4 class="text-normal text-center">Last Login Date : Monday, October 31, 2022  |  08:00</h4>
+                <h4 class="text-normal text-center">Last Logout Date : Monday, October 31, 2022  |  08:00</h4>
+            </div>
+            <div class="border main-container flex-gap">
+                <!--Registered Courses-->
+                <h3>Registered Courses</h3>
+                <table>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Data Structures and Algorithms III</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Rapid Application Development</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Mathematical Methods</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Software Engineering III</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Computer Networks</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Functional Programming</td>
+                    </tr>
+                    <tr>
+                        <td>SCS2201</td>
+                        <td>Programming Language Concepts</td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<script>
+    var modal = document.getElementById("modal");
+    var btn = document.getElementById("edit");
+    var span = document.getElementsByClassName("close");
+
+    btn.onclick = function (){
+        modal.style.display = "block";
+    }
+    span.onclick = function (){
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target === modal) {
             modal.style.display = "none";
         }
-        window.onclick = function(event) {
-            if (event.target === modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
-</body>
-</html>
+    }
+</script>
+
