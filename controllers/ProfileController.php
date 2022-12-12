284769f1e840bbe75ed2483343cce58efb922dd1
Ramindu Walgama <raminduw200@gmail.com>
2022-12-13 01:21:28 +0530

    Add uplaodCSV post request
    Check the opened csv file is a valid file and read each line

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\model\Student;
 
 class ProfileController extends Controller
 {
@@ -11,5 +12,37 @@ class ProfileController extends Controller
     {
         $profile = unserialize($_SESSION['user']);
         return $this->render('profile', $profile->flatten());
+    }
+
+    // POST request
+    public function uploadCSV()
+    {
+        $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+        if (!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)) {
+            if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+                $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+                fgetcsv($csvFile);
+                while (($line = fgetcsv($csvFile)) !== FALSE) {
+                    if (Student::validateUserAttributes(
+                        $line[0], // regNo
+                        $line[1], // firstName
+                        $line[2], // lastName
+                        $line[3], // email
+                        $line[4], // contactNo
+                        $line[5], // indexNo
+                        $line[6]  // degreeProgramCode
+                    )) {
+                        # TODO: check if student already exists in the database
+                        #       else insert
+                    }
+                }
+                fclose($csvFile);
+                $qstring = '?status=succ';
+            } else {
+                $qstring = '?status=err';
+            }
+        } else {
+            $qstring = '?status=invalid_file';
+        }
     }
 }
