e3e17e687336bb03eede96f1455c80da9db9e2b9
Dilanga-Harshani <aadilangaharshani@gmail.com>
2022-12-29 09:44:21 +0530

    Modified to retrieve a single course

--- controllers/CourseController.php
+++ controllers/CourseController.php
@@ -4,13 +4,15 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\model\Course;
 
 class CourseController extends Controller
 {
     public function displayCourses()
     {
         $user = unserialize($_SESSION['user']);
-        $course = $user->getRegNo();
-        return $this->render('course_overview', $course);
+        $userRegno = $user->getRegNo();
+        $course = Course::fetchCourseFromDb($userRegno);
+        return $this->render('course_overview', ['course' => $course]);
     }
 }
