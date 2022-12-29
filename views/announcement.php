120c52cbe7765d746aafb0908ff1df4380bfd3ed
anjanasilva99 <akirindasilva@gmail.com>
2022-12-29 10:27:19 +0530

    Add css for 'publish' button

--- views/announcement.php
+++ views/announcement.php
@@ -6,7 +6,7 @@
             <div class="topic_container_add grid v-center h-justify">
                 <textarea id="heading_textarea" name="heading" placeholder="Add Announcement Heading..."
                           class="add_headline text-bold v-center text-justify" id="" wrap="hard"></textarea>
-                <button class="btn h-center v-center">Publish</button>
+                <button class="btn confirm-btn h-center v-center">Publish</button>
             </div>
             <div class="announcement_card_inside border">
                 <div class="container_heading grid h-justify v-center">
@@ -18,8 +18,7 @@
                 </div>
             </div>
         </div>
-<!---->
-<!---->
+<!-- edit and delete button-->
 <!--    <div class="announcement_card border">-->
 <!--        <div class="topic_container grid v-center h-justify container_edit_delete" >-->
 <!--            <h4 class="heading_content text-bold text-justify">Lorem ipsum dolor sit amet jsgfj bdkjfhsk bjks mnkhlk akfk nkafk nnsfk kjaf afgajfga bjfj fkh nskhfk skhf.</h4>-->
