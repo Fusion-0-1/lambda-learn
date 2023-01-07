d538c4c08040d141f9cccd7efe2b56da59891d46
Inuri Lavanya <lilh99lilh@gmail.com>
2023-01-07 19:32:22 +0530

    Modify the function addTopic and create function getTopicNumberFromID

--- views/course_initialization.php
+++ views/course_initialization.php
@@ -9,7 +9,7 @@
         <h4>Course Topics</h4><hr><br>
         <div class="flex flex-wrap h-justify">
             <div id="topic" class="flex flex-wrap h-evenly flex-gap">
-                <input id="input_topic-1" class="input input-topic flex flex-gap" placeholder="Add new topic">
+                <input id="input_topic_1" class="input input-topic flex flex-gap" placeholder="Add new topic" onkeyup="addTopic(this)">
             </div>
         </div>
         <div class="flex margin-top h-center">
@@ -20,10 +20,10 @@
     <div class="border main-container flex-gap">
         <h4>Course Sub Topics</h4><hr><br>
         <div id="course-topic" class="flex flex-gap flex-wrap h-center">
-            <div class="border container-course-topic v-center flex flex-gap v-center flex-column">
+            <div id="sub_topic_list_1" class="border container-course-topic v-center flex flex-gap v-center flex-column">
                 <h5 id="topic-1" class="text-center remove-space">Add new topic...</h5>
                 <div id="subtopic-1" class="border container-course-sub-topic v-center padding-none flex flex-column">
-                    <input id="sub_topic-1-1" class="input input-subtopic flex" placeholder="Add new sub topic..." disabled>
+                    <input class="input input-subtopic flex" placeholder="Add new sub topic...">
                 </div>
                 <button id="1" class="btn-circle" onclick="addSubTopic(this.id)" disabled><i class="fa-solid fa-plus"></i></button>
             </div>
@@ -51,46 +51,76 @@
 
 
 <script>
-    var count = 1;
-    var subtopic_count = 1;
-    var id = document.getElementById("input_topic-"+count);
-    id.addEventListener("input", addInput);
 
-    function addInput(){
-        document.getElementById("topic-1").innerHTML = document.getElementById("input_topic-1").value;
-        document.getElementById("initialize").removeAttribute("disabled")
-        document.getElementById(""+count).removeAttribute("disabled");
-        document.getElementById("sub_topic-1-1").removeAttribute("disabled")
+/*
+    0. create function to set as onchange
+    1. pass element(element id) to the onchange function as parameter
+    2. IFF element id + 1 not exists x(use try catch)
+           element id + 1 -> create input field from that
+           element -> set onchange function (0.) with current element as parameter <-------------------- Done upto here
+           same way : create subtopic from that
+       ELSE
+            update existing element (update sub-header)
+            IF header_length == 0
+                delete sub topic element
+    2.1 Delete empty input fields in the middle.
+        1. get all input field as a list : document.getElementById('topic').getElementsByTagName('input')[i]
+        2. filter only empty fields -> emptyFieldsArr
+        3. IF length(emptyFieldsArr) > 1:
+                delete all elements in emptyFieldsArr[:length(emptyFieldsArr)-1]
 
-        if(id.value.length !== 0){
-            count++;
-            var new_input = document.createElement("INPUT");
+-----------------------------------------------------------------------------------
+    NOT NEEDED : 3. 2. element -> set onchange function (0.) with current element as parameter
+ */
+
+function getTopicNumberFromID(topic_id){
+    let topic_arr = topic_id.split('_');
+    return parseInt(topic_arr[topic_arr.length-1]);
+}
+
+function addTopic(topic_element){
+    let topic_id = getTopicNumberFromID(topic_element.id);
+    let topic_elements = document.getElementById('topic').getElementsByTagName('input');
+    let exists = false;
+    for (let i=0; i < topic_elements.length; i++) {
+        if (getTopicNumberFromID(topic_elements[i].id) === topic_id + 1) {
+            exists = true;
+            break;
+        }
+    }
+    let new_input;
+    for (let i=0; i < topic_elements.length; i++) {
+        if (!exists) {
+            document.getElementById("initialize").removeAttribute("disabled");
+            document.getElementById(""+(topic_id)).removeAttribute("disabled");
+
+            new_input = document.createElement("INPUT");
             new_input.setAttribute("type", "text");
             new_input.setAttribute("placeholder", "Add new topic");
             new_input.setAttribute("class", "input input-topic flex flex-gap");
-            new_input.setAttribute("id", "input_topic-"+count);
+            new_input.setAttribute("id", "input_topic_" + (topic_id + 1));
+            new_input.setAttribute("onkeyup", "addTopic(this)");
             document.getElementById('topic').appendChild(new_input);
-            id = document.getElementById("input_topic-"+count);
 
             var topic_container = document.createElement('div');
             topic_container.classList.add("border", "container-course-topic", "v-center", "flex", "flex-gap", "v-center", "flex-column");
 
             var heading = document.createElement('h5');
             heading.innerText = "Add topic"
-            heading.setAttribute("id", "topic-"+count);
+            heading.setAttribute("id", "topic-"+(topic_id+1));
             heading.classList.add("text-center", "remove-space");
 
             var subtopic_container = document.createElement('div');
-            subtopic_container.setAttribute("id", "subtopic-"+count);
+            subtopic_container.setAttribute("id", "subtopic-"+(topic_id+1));
             subtopic_container.classList.add("border", "container-course-sub-topic", "v-center", "padding-none", "flex", "flex-column");
 
             var input = document.createElement("input");
             input.classList.add("input", "input-subtopic", "flex");
-            input.setAttribute("id", "sub_topic-"+count+"-"+subtopic_count);
             input.setAttribute("placeholder", "Add new sub topic...");
 
             var add_button = document.createElement("button");
-            add_button.setAttribute("id", ""+count);
+            add_button.setAttribute("id", ""+(topic_id+1));
+            add_button.disabled = true;
             add_button.classList.add("class", "btn-circle", "fa-solid", "fa-plus");
 
             add_button.onclick = function(){ return addSubTopic(this.id)};
@@ -101,10 +131,84 @@
             topic_container.appendChild(add_button);
             document.getElementById("course-topic").appendChild(topic_container);
 
-            id.addEventListener("input", addInput);
+            console.log(new_input.id);
+            break; // maybe need to remove this
         }
-        //TODO: remove input field if empty
+        else{
+            document.getElementById("topic-"+topic_id).innerHTML = document.getElementById("input_topic_"+topic_id).value;
+        }
     }
+    // DELETING EMPTY FIELDS FROM THE TOPIC INPUTS
+    topic_elements = document.getElementById('topic').getElementsByTagName('input');
+    let empty_fields = [];
+    for (let i=0; i < topic_elements.length; i++) {
+        if (topic_elements[i].value === "") {
+            empty_fields.push(topic_elements[i]);
+            console.log(topic_elements[i]);
+        }
+    }
+    if (empty_fields.length > 1) {
+        for (let i = 0; i < empty_fields.length-1; i++) {
+            empty_fields[i].remove();
+        }
+    }
+}
+    // var count = 1;
+    // var subtopic_count = 1;
+    // var id = document.getElementById("input_topic-"+count);
+    // id.addEventListener("input", addInput);
+
+    // function addInput(){
+    //     document.getElementById("topic-"+count).innerHTML = document.getElementById("input_topic-"+count).value;
+    //     console.log(document.getElementById("input_topic-"+count));
+    //     document.getElementById("initialize").removeAttribute("disabled");
+    //     document.getElementById(""+count).removeAttribute("disabled");
+    //     document.getElementById("sub_topic-1-1").removeAttribute("disabled");
+    //
+    //     if(id.value.length !== 0){
+    //         count++;
+    //         var new_input = document.createElement("INPUT");
+    //         new_input.setAttribute("type", "text");
+    //         new_input.setAttribute("placeholder", "Add new topic");
+    //         new_input.setAttribute("class", "input input-topic flex flex-gap");
+    //         new_input.setAttribute("id", "input_topic-"+count);
+    //         document.getElementById('topic').appendChild(new_input);
+    //         id = document.getElementById("input_topic-"+count);
+    //
+    //         var topic_container = document.createElement('div');
+    //         topic_container.classList.add("border", "container-course-topic", "v-center", "flex", "flex-gap", "v-center", "flex-column");
+    //
+    //         var heading = document.createElement('h5');
+    //         heading.innerText = "Add topic"
+    //         heading.setAttribute("id", "topic-"+count);
+    //         heading.classList.add("text-center", "remove-space");
+    //
+    //         var subtopic_container = document.createElement('div');
+    //         subtopic_container.setAttribute("id", "subtopic-"+count);
+    //         subtopic_container.classList.add("border", "container-course-sub-topic", "v-center", "padding-none", "flex", "flex-column");
+    //
+    //         var input = document.createElement("input");
+    //         input.classList.add("input", "input-subtopic", "flex");
+    //         input.setAttribute("id", "sub_topic-"+count+"-"+subtopic_count);
+    //         input.setAttribute("placeholder", "Add new sub topic...");
+    //
+    //         var add_button = document.createElement("button");
+    //         add_button.setAttribute("id", ""+count);
+    //         add_button.classList.add("class", "btn-circle", "fa-solid", "fa-plus");
+    //
+    //         add_button.onclick = function(){ return addSubTopic(this.id)};
+    //
+    //         topic_container.appendChild(heading);
+    //         subtopic_container.appendChild(input);
+    //         topic_container.appendChild(subtopic_container);
+    //         topic_container.appendChild(add_button);
+    //         document.getElementById("course-topic").appendChild(topic_container);
+    //
+    //         id.addEventListener("input", addInput);
+    //     }
+    //     //TODO: remove input field if empty
+    // }
+    let subtopic_count = 1;
 
     function addSubTopic(clicked_id){
         subtopic_count++;
