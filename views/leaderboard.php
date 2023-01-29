8d499250594925fbc1de68426f8129c252a35802
anjanasilva99 <akirindasilva@gmail.com>
2023-01-29 11:46:09 +0530

    completed veiw of leaderboard page
    completed view of the leaderboard page using html, css and javascript and mobile responsive.

--- views/leaderboard.php
+++ views/leaderboard.php
@@ -1,1 +1,25 @@
-<?php
+<link rel="stylesheet" href="css/leaderboard.css">
+
+<div class="main-container border v-center flex-gap responsive-container">
+    <h2 class="main_heading text-center">Leader Board</h2>
+    <div class="card">
+        <div class="cards_heading text-bold text-center grid head">
+            <div>Rank</div>
+            <div>Students Name</div>
+            <div>Index Number</div>
+            <div>Point</div>
+        </div>
+        <div class="cards_heading text-bold text-center text-normal grid cards_content">
+            <div>1</div>
+            <div>N.S. Perera</div>
+            <div>20001789</div>
+            <div>6789</div>
+        </div>
+        <div class="cards_heading text-bold text-center text-normal grid cards_content1">
+            <div>1</div>
+            <div>N.S. Perera</div>
+            <div>20001789</div>
+            <div>6789</div>
+        </div>
+    </div>
+</div>
