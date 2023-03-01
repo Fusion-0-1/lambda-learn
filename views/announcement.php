02a48ddce0c31f0cec2c88346bba04edd17c2ee8
anjanasilva99 <akirindasilva@gmail.com>
2023-03-01 23:35:40 +0530

    The line that reduced the time by one hour was removed. This change is related to the 127-insert-siteannouncement branch

--- views/announcement.php
+++ views/announcement.php
@@ -3,62 +3,52 @@
 <div class="main-container border v-center flex-gap responsive-container">
     <h2 class="text-center course_name">Site Announcements</h2>
     <?php if ($_SESSION['user-role'] == 'Admin' or $_SESSION['user-role'] == 'Coordinator') {?>
-        <div class="announcement_card border">
+        <form method="post" action="/site_announcement" class="announcement_card border">
             <div class="topic_container_add grid v-center h-justify">
                 <textarea id="heading_textarea" name="heading" placeholder="Add Announcement Heading..."
                           class="add_headline text-bold v-center text-justify" id="" wrap="hard"></textarea>
 
-                <button class="btn confirm-btn h-center v-center">Publish</button>
-
+                <button id="publishbtn" class="btn confirm-btn h-center v-center">Publish</button>
             </div>
             <div class="announcement_card_inside border">
                 <div class="container_heading grid h-justify v-center">
-                    <input type="text" name="name" placeholder="Enter Your Name... " class="add_name">
-                    <div class="view_lecture_name_and_date_time text-right">2018-02-21 12:00:00</div>
+                    <div class="add_name">                    <?php
+                        $profile = unserialize($_SESSION['user']);
+                        echo $profile->getFirstName()." ".$profile->getLastName();
+                        ?></div>
                 </div>
                 <div  class="add_announcement_content_div">
                     <textarea id="content_textarea" name="content" placeholder="Add Announcement content...   " class="add_announcement_content text-justify"></textarea>
                 </div>
             </div>
-        </div>
+        </form>
         <?php } ?>
-<!-- edit and delete button-->
-
-    <div class="announcement_card border">
-        <div class="topic_container grid v-center h-justify container_edit_delete" >
-            <h4 class="heading_content text-bold text-justify">Rescheduling the tutorial.</h4>
-            <?php if ($_SESSION['user-role'] == 'Admin' or $_SESSION['user-role'] == 'Coordinator') {?>
-                <div class="edit_delete_timeremaining grid v-center">
-                    <div class="edit_time"><b>30</b><span> mins left</span></div>
-                    <a href="" class="deletebtn"><img src="./images/announcement/Delete.png" alt="Delete image"></a>
-                    <a href="" class="editbtn"><img src="./images/announcement/Edit.png" alt="Edit image"></a>
-                </div>
-            <?php } ?>
-        </div>
-        <div class="announcement_card_inside border">
-            <div class="container_heading grid h-justify v-center">
-                <div class="view_lecture_name_and_date_time">Mr. Nimal Kodikara</div>
-                <div class="view_lecture_name_and_date_time text-right">2018-02-21 12:00:00</div>
-            </div>
-            <p class="text-justify">
-                Dear Students,
-
-                As per your requests, today's tutorial session is rescheduled to Monday, 13th of February 2023 from 10.00 AM to 12.00 PM at the Mini Auditorium. You will have the first in-class assignment on Tuesday, 14th of February 2023 as informed earlier.
-
-                Thank you
-            </p>
-        </div>
-    </div>
 
     <?php foreach ($announcements as $ann) { ?>
         <div class="announcement_card border">
             <div class="topic_container grid v-center h-justify container_edit_delete">
                 <h4 class="heading_content text-bold text-justify"><?php echo $ann->getHeading()?></h4>
+                <?php if ($_SESSION['user-role'] == 'Admin' or $_SESSION['user-role'] == 'Coordinator') {?>
+                    <div class="edit_delete_timeremaining grid v-center">
+                        <div class="edit_time" id="timeremaning"><b>30</b><span> mins left</span></div>
+                        <a href="" class="deletebtn" id="deletebtn"><img src="./images/announcement/Delete.png" alt="Delete image"></a>
+                        <a href="" class="editbtn" id="editbtn"><img src="./images/announcement/Edit.png" alt="Edit image"></a>
+                    </div>
+                <?php } ?>
             </div>
             <div class="announcement_card_inside border">
                 <div class="container_heading grid h-justify v-center">
                     <div class="view_lecture_name_and_date_time">Mr. Nimal Kodikar</div>
-                    <div class="view_lecture_name_and_date_time text-right"><?php echo $ann->getPublishDate()?></div>
+                    <div class="view_lecture_name_and_date_time text-right">
+                        <?php
+                            $utcTime = $ann->getPublishDate();
+                            $sriLankanTimezone = new DateTimeZone('Asia/Colombo');
+                            $date = new DateTime($utcTime, new DateTimeZone('UTC'));
+                            $date->setTimezone($sriLankanTimezone);
+                            $sriLankanDateAndTime = $date->format('l, F d, Y | H:i');
+                            echo $sriLankanDateAndTime;
+                        ?>
+                    </div>
                 </div>
                 <p class="text-justify">
                     <?php echo $ann->getContent()?>
