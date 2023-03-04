659ba9fcb071248e264a8d5f88181d71580b8d66
anjanasilva99 <akirindasilva@gmail.com>
2023-03-04 10:24:45 +0530

    Add controller dispay and insert for course_announcement

--- controllers/AnnouncementController.php
+++ controllers/AnnouncementController.php
@@ -5,7 +5,7 @@ namespace app\controllers;
 use app\core\Controller;
 use app\core\Request;
 use app\model\SiteAnnouncement;
-
+use app\model\CourseAnnouncement;
 
 
 class AnnouncementController extends Controller
@@ -19,10 +19,23 @@ class AnnouncementController extends Controller
         );
     }
 
-    public function createAnnouncements(Request $request)
+    public function displayCourseAnnouncements(Request $request)
+    {
+        $body = $request->getBody();
+        $params['course_code'] = $body['course_code'];
+        $params['announcements'] = CourseAnnouncement::getCourseAnnouncements();
+        return $this->render(
+            view: 'course/course_announcement',
+            params: $params
+
+        );
+    }
+
+    public function createSiteAnnouncements(Request $request)
     {
         $body = $request->getBody();
         $profile = unserialize($_SESSION['user']);
+
         if ($_SESSION['user-role'] == 'Coordinator') {
             $site_announcement = SiteAnnouncement::createNewAnn(
                 heading: $body['heading'],
@@ -36,7 +49,24 @@ class AnnouncementController extends Controller
                 adminRegNo: $profile->getRegNo()
             );
         }
-        $site_announcement->insert();
+
+        $site_announcement->SiteAnnouncementInsert();
         header("Location: /site_announcement");
+    }
+
+
+    public function createCourseAnnouncements(Request $request)
+    {
+        $body = $request->getBody();
+        $profile = unserialize($_SESSION['user']);
+        $course_announcement = CourseAnnouncement::createNewAnn(
+            heading: $body['heading'],
+            content: $body['content'],
+            lecRegNo: $profile->getRegNo(),
+            courseCode: $body['course_code']
+        );
+
+        $course_announcement->CourseAnnouncementInsert();
+        header("Location: /course_announcement?course_code=".$body['course_code']);
     }
 }
