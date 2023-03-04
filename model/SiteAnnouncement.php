b25460084c09a798ebf5eedd89ffa8e0a39ad5bc
anjanasilva99 <akirindasilva@gmail.com>
2023-03-04 10:33:08 +0530

    modify, model of site announcement.

--- model/SiteAnnouncement.php
+++ model/SiteAnnouncement.php
@@ -9,28 +9,32 @@ class SiteAnnouncement extends Announcement
     private string $adminRegNo;
     private string $cordRegNo;
 
+
     private function __construct()
     {
 
     }
     public static function fetchAnnFromDb(int $id){
+        $siteAnnouncement = new SiteAnnouncement();
         $table = self::getSiteAnnouncementData($id);
 
-        $siteAnnouncement = new SiteAnnouncement();
-        $siteAnnouncement->announcementId = $table['announcement_id'];
-        $siteAnnouncement->heading = $table['heading'];
-        $siteAnnouncement->content = $table['content'];
-        $siteAnnouncement->publishDate = $table['publish_date'];
-        $siteAnnouncement->adminRegNo = $table['admin_reg_no'];
-        $siteAnnouncement->cordRegNo = $table['cord_reg_no'];
+            $siteAnnouncement->announcementId = $table['announcement_id'];
+            $siteAnnouncement->heading = $table['heading'];
+            $siteAnnouncement->content = $table['content'];
+            $siteAnnouncement->publishDate = $table['publish_date'];
+            $siteAnnouncement->adminRegNo = $table['admin_reg_no'];
+            $siteAnnouncement->cordRegNo = $table['cord_reg_no'];
 
-        return $siteAnnouncement;
+            return $siteAnnouncement;
     }
-    public static function createNewAnn(int $announcementId, string $heading, string $content, string $publishDate,
-                                        $adminRegNo, $cordRegNo){
+    public static function createNewAnn(string $heading, string $content, string $publishDate='',
+                                        $adminRegNo='', $cordRegNo='', int $announcementId=null){
+
 
         $announcement = new SiteAnnouncement();
-        $announcement->announcementId = $announcementId;
+        if ($announcementId != null){
+            $announcement->announcementId = $announcementId;
+        }
         $announcement->heading = $heading;
         $announcement->content = $content;
         $announcement->publishDate = $publishDate;
@@ -39,28 +43,50 @@ class SiteAnnouncement extends Announcement
 
         return $announcement;
     }
+    //--------------------Display Site-announcement------------------------------
     private static function getSiteAnnouncementData($id): array
     {
         return parent::getAnnouncementData($id,'SiteAnnouncement');
     }
+
+
     public static function getSiteAnnouncements(): array
     {
         $siteAnnouncements = [];
         $results = Application::$db->select(
-            table: 'SiteAnnouncement'
+            table: 'SiteAnnouncement',
+            order: 'announcement_id DESC'
         );
         while ($ann = Application::$db->fetch($results)){
              $siteAnnouncements[] = self::createNewAnn(
-                 (int)$ann['announcement_id'],
                  $ann['heading'],
                  $ann['content'],
                  $ann['publish_date'],
                  $ann['admin_reg_no'],
-                 $ann['cord_reg_no']
+                 $ann['cord_reg_no'],
+                 (int)$ann['announcement_id'],
              );
         }
+
         return $siteAnnouncements;
     }
+
+    //---------------Insert SiteAnnouncement------------------
+
+    public function SiteAnnouncementInsert()
+    {
+        Application::$db->insert(
+            table: 'SiteAnnouncement',
+            values: [
+                'heading' => $this->heading,
+                'content' => $this->content,
+                'publish_date' => $this->publishDate ?: date('Y-m-d H:i:s'),
+                'admin_reg_no' => $this->adminRegNo ?? '',
+                'cord_reg_no' => $this->cordRegNo ?? ''
+            ]
+        );
+    }
+
 
     /**
      * @return string
