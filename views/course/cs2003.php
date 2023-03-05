5d1df6b0993ebae51cee7a9045fb48c027aa1e84
anjanasilva99 <akirindasilva@gmail.com>
2023-03-05 10:54:05 +0530

    Passing the course code through the URL

--- views/course/cs2003.php
+++ views/course/cs2003.php
@@ -115,7 +115,7 @@
         <div class="inner-secondary-container border border-radius flex flex-column">
             <div class="flex flex-row h-justify v-center">
                 <h5> Submissions </h5>
-                <a href="/submissions" class="hyperlink"> View all </a>
+                <a href="/submissions?course_code=<?php echo ('CS 2001')?>" class="hyperlink"> View all </a>
             </div>
             <button class="inner-container border-radius text-left" id="submission1"> Submission 3 - Greedy Alogrothms </button>
             <button class="inner-container border-radius text-left"> Submission 2 - Greedy Alogorithms </button>
