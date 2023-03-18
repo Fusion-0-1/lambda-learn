b620463610a586febeef1260e5b8dbfedd3bbe3a
Ramindu Walgama <raminduw200@gmail.com>
2023-03-18 12:49:02 +0530

    Add uploadCSV post request

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\core\CSVFile;
 use app\core\Request;
 
 class ReportController extends Controller
@@ -15,11 +16,31 @@ class ReportController extends Controller
         );
     }
 
-    public function uploadAttendance(Request $request){
+    public function uploadAttendance(Request $request)
+    {
         if ($request->isGet()){
             return $this->render(
                 view: '/attendance_upload',
                 allowedRoles: ['Admin']
+            );
+        } elseif ($request->isPost()) {
+            $file = new CSVFile($request->getFile());
+            $output = $file->readCSV(updateAttendance: true);
+            # if csv header course codes are invalid, $output will be a false.
+            # if regNo are invalid, invalid regNos will be assigned to $output
+            # if all are correct, $output will be an empty array
+            if ($output != false) {
+                return $this->render(
+                    view: '/attendance_upload',
+                    allowedRoles: ['Admin'],
+                    params: ['errors' => $output]
+                );
+            }
+            $body['success_mssg'] = true;
+            return $this->render(
+                view: '/attendance_upload',
+                allowedRoles: ['Admin'],
+                params: $body
             );
         }
     }
