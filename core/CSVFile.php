d7103922d9d1349ef1c57642df6a8c8c29b94720
Ramindu Walgama <raminduw200@gmail.com>
2023-03-18 12:25:31 +0530

    Add read attendance csv functionality
    Comprimise previous account creation functionality into readCSV function and split the uncommon parts of updateAttendance and readUserData csv into two separate function and call inside the readCSV function

--- core/CSVFile.php
+++ core/CSVFile.php
@@ -2,6 +2,9 @@
 
 namespace app\core;
 
+use app\model\Course;
+use app\model\User\Student;
+
 class CSVFile
 {
     private string $filename;
@@ -22,42 +25,102 @@ class CSVFile
 
 
     /**
-     * @description Read the CSV file and unwrapped the columns data. This will categorize data into
-     * valid, update and invalid data. Update array contains the students who are existing in the database,
-     * invalid array contains students index numbers which students have incorrect data (one or more attributes of the
-     * student is invalid).
-     * @param $constructor
+     * @description Read the CSV file
+     * two functionalities wrapped into this function.
+     *  1. Read user account creation csv upload
+     *       Read the csv and unwrapped the columns' data. This will categorize data into valid, update and invalid
+     *       data. Update array contains the students who are existing in the database, invalid array contains students
+     *       index numbers which students have incorrect data (one or more attributes of the student is invalid).
+     *       @param $constructor: constructor of the account creation object. [class, 'constructor of factory pattern']
+     *                  example : [Student::class, 'createNewStudent'];
+     *       @param bool $readUserData: true (this will enable read user data function)
+     *  2. Update Student attendance.
+     *       This will read attendance containing csv file. Will return false if course codes provided are wrong.
+     *       This will update student update for each course as specified in the csv file. 10 points will be given for
+     *       leaderboard ranking.
+     *       @param bool $updateAttendance: true (This should be true if want to enable this feature).
      * @return array|bool
      */
-    public function readUserCSV($constructor): array|bool
+    public function readCSV($constructor = null, $readUserData = false, $updateAttendance = false): bool|array
     {
-        $valid = [];
-        $invalid = [];
-        $update = [];
-
         if (!empty($this->filename) && in_array($this->filetype, self::csvMimes)) {
             if (is_uploaded_file($this->filepath)) {
                 $csvFile = fopen($this->filepath, 'r');
-                fgetcsv($csvFile);
-                while (($line = fgetcsv($csvFile)) !== FALSE) {
-                    $unwrappedData = User::unwrapData($line);
-                    if (User::validateUserAttrFromArray($unwrappedData)) {
-                        $newUser = call_user_func($constructor, $unwrappedData);
-                        if (!User::userExists($newUser->getRegNo())) {
-                            $valid[] = $newUser;
-                        } else {
-                            $update[] = $newUser;
-                        }
-                    } else {
-                        $invalid[] = $line[0]; // regNo
-                    }
+                if ($readUserData) {
+                    $output = $this->readUserData($constructor, $csvFile);
+                } else if ($updateAttendance){
+                    $output = $this->updateAttendance($csvFile);
                 }
                 fclose($csvFile);
             } else {
                 return false;
             }
         }
+        return $output;
+    }
 
-    return ['valid' => $valid, 'invalid' => $invalid, 'update' => $update];
+
+    /**
+     * @description Create user accounts. This function must be used inside readCSV. This is a passive function.
+     * i.e. function itself can not run.
+     * @param $csvFile: csvFile which was read by inbuilt fopen(). This is used in readCSV file function.
+     * @param $constructor: constructor of the account creation object. [class, 'constructor of factory pattern']
+     *            example : [Student::class, 'createNewStudent'];
+     * @return bool|array
+     */
+    public function readUserData($constructor, $csvFile): array
+    {
+        $valid = [];
+        $invalid = [];
+        $update = [];
+        $header = fgetcsv($csvFile); # contains the column names
+
+        while (($line = fgetcsv($csvFile)) !== FALSE) {
+            $unwrappedData = User::unwrapData($line);
+            if (User::validateUserAttrFromArray($unwrappedData)) {
+                $newUser = call_user_func($constructor, $unwrappedData);
+                if (!User::userExists($newUser->getRegNo())) {
+                    $valid[] = $newUser;
+                } else {
+                    $update[] = $newUser;
+                }
+            } else {
+                $invalid[] = $line[0]; // regNo
+            }
+        }
+        return ['valid' => $valid, 'invalid' => $invalid, 'update' => $update];
+    }
+
+
+    /**
+     * @description Update student attendance. This function must be used inside readCSV. This is a passive function.
+     * i.e. function itself can not run.
+     * @param $csvFile: csvFile which was read by inbuilt fopen(). This is used in readCSV file function.
+     * @return bool|array
+     */
+    public function updateAttendance($csvFile): bool|array
+    {
+        $invalid = [];
+        $header = fgetcsv($csvFile); # contains the column names
+        array_shift($header); # Header first element is "Index". So, remove it.
+//        var_dump($header);
+        for ($i = 0; $i < count($header); $i++) {
+            $header[$i] = trim($header[$i]);
+            if (!Course::checkExists($header[$i])) {
+                return false;
+            }
+        }
+        while (($line = fgetcsv($csvFile)) !== false) {
+            # [2020/cs/0001, 0, 1, 0, 0, 0, 1, 1, 1]
+            $regNo = trim(array_shift($line));
+            if (User::userExists($regNo)) {
+                $student = Student::fetchStuFromDb($regNo);
+                $student->updateAllCoursesAttendance($header, $line);
+            } else {
+                $invalid[] = $regNo;
+            }
+        }
+//        var_dump($invalid);
+        return $invalid;
     }
 }
