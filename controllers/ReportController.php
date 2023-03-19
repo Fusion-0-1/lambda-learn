23fe2115ce1be55e499da331d8ac5f792534ba60
Ramindu Walgama <raminduw200@gmail.com>
2023-03-19 21:36:13 +0530

    Change CSV file save location

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -21,7 +21,8 @@ class ReportController extends Controller
         if ($request->isGet()){
             return $this->render(
                 view: '/attendance_upload',
-                allowedRoles: ['Admin']
+                allowedRoles: ['Admin'],
+                params: ['csvFiles' => CSVFile::getAttendanceReports()]
             );
         } elseif ($request->isPost()) {
             $body = $request->getBody();
@@ -29,16 +30,20 @@ class ReportController extends Controller
 
             $output = $file->readCSV(
                 updateAttendance: true,
-                location: '../CSV Files/User Uploads/Attendance/' . $body['date']
+                location: 'User Uploads/Attendance/' . $file->getFilename() . "_" . $body['date']
             );
+            // get files after upload
+            $body['csvFiles'] = CSVFile::getAttendanceReports();
+
             # if csv header course codes are invalid, $output will be a false.
             # if regNo are invalid, invalid regNos will be assigned to $output
             # if all are correct, $output will be an empty array
             if (count($output) > 0) {
+                $body['errors'] = $output;
                 return $this->render(
                     view: '/attendance_upload',
                     allowedRoles: ['Admin'],
-                    params: ['errors' => $output]
+                    params: $body
                 );
             }
             $body['success_mssg'] = true;
