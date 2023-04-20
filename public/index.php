024a528a4b5241c404c3ea6e872268ac3cc352e9
anjanasilva99 <akirindasilva@gmail.com>
2023-04-20 20:38:52 +0530

    Changed function name

--- public/index.php
+++ public/index.php
@@ -42,8 +42,7 @@ $app->router->get('/utilization', [SummaryViewController::class, 'displayUtiliza
 $app->router->get('/submissions', [CourseController::class, 'displayAllSubmissions']);
 $app->router->get('/marks_upload', [CourseController::class, 'displayCourseMarkUpload']);
 $app->router->post('/submissions', [CourseController::class, 'CreateSubmission']);
-$app->router->post('/submission_visibility', [CourseController::class, 'SubmissionVisibility']);
-
+$app->router->post('/submission_visibility', [CourseController::class, 'changeSubmissionVisibility']);
 
 $app->router->get('/leaderboard', [LeaderboardController::class, 'displayLeaderboard']);
 
