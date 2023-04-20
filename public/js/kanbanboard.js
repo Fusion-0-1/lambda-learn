e572c19eb8fe137e5ff1270a656a694dced049d5
Dilanga-Harshani <aadilangaharshani@gmail.com>
2023-04-20 18:01:44 +0530

    Kanbanupdate function and operations of card edit modal added

--- public/js/kanbanboard.js
+++ public/js/kanbanboard.js
@@ -7,9 +7,9 @@ const newbtndone = document.getElementById('card-add-done');
 
 const newmodal = document.getElementById('card-add-modal');
 const savebutton = document.getElementById('card-add-save');
-const cardtitle = document.getElementById('card-header-modal'); 
-const cancelbtnnew = document.getElementById('card-save-cancel');  
-        
+const cardtitle = document.getElementById('card-header-modal');
+const cancelbtnnew = document.getElementById('card-save-cancel');
+
 const radiotodo = document.getElementById('radio-todo');
 const radioinprogress = document.getElementById('radio-inprogress')
 const radiodone = document.getElementById('radio-done');
@@ -26,26 +26,27 @@ newbtndone.addEventListener('click', function () {
     newmodal.style.display = 'block';
     radiodone.innerHTML = '<input type="radio" name="card-status" value="Done" checked> Done';
 });
-savebutton.addEventListener('click', function () {
-    if (cardtitle == NULL) {
-        cardtitle.innerHTML.value = "Please enter a card title"
-    }
-    else {
-        newmodal.style.display = 'none';
-    }
-});
+// savebutton.addEventListener('click', function () {
+//     if (cardtitle === NULL) {
+//         cardtitle.innerHTML.value = "Please enter a card title"
+//     }
+//     else {
+//         newmodal.style.display = 'none';
+//     }
+// });
 cancelbtnnew.addEventListener('click', function () {
     newmodal.style.display = 'none';
-})
+});
+
 window.onclick = function(event) {
-    if (event.target == newmodal) {
+    if (event.target === newmodal) {
         newmodal.style.display = 'none';
     }
 }
 document.addEventListener('dragstart', e=> {
     if(e.target.classList.contains('draggable')) {
         e.target.classList.add('dragging');
-    }       
+    }
 });
 document.addEventListener('dragend', e=> {
     if(e.target.classList.contains('draggable')) {
@@ -66,7 +67,7 @@ droppables.forEach(droppable=> {
     });
 });
 function getClosestFrontSibling(droppable, draggingY) {
-    const siblings = droppable.querySelectorAll('.draggable:not(.dragging');
+    const siblings = droppable.querySelectorAll('.draggable:not(.dragging)');
     let result;
     for (const sibling of siblings) {
         const box = sibling.getBoundingClientRect();
@@ -78,4 +79,31 @@ function getClosestFrontSibling(droppable, draggingY) {
         }
     }
     return result;
+}
+
+const editmodal = document.getElementById('card-edit-modal');
+const editbtn = document.getElementsByName('card-update');
+const saveeditbutton = document.getElementById('card-edit-save');
+const canceleditbtn = document.getElementById('card-edit-cancel');
+
+function kanbanupdate(taskid, tasktitle, taskdescription, taskstate, taskdeadline) {
+    editmodal.style.display = 'block';
+    document.getElementById('card-header-edit-modal').value = tasktitle;
+    document.getElementById('card-body-edit-modal').value = taskdescription;
+    document.getElementById('card-deadline-edit-modal').value = taskdeadline;
+    document.getElementById('card-state').value = taskstate;
+    document.getElementById('card-id').value = taskid;
+}
+
+saveeditbutton.addEventListener('click', function () {
+        editmodal.style.display = 'none';
+});
+canceleditbtn.addEventListener('click', function () {
+    editmodal.style.display = 'none';
+});
+
+window.onclick = function(event) {
+    if (event.target === editmodal) {
+        editmodal.style.display = 'none';
+    }
 }
