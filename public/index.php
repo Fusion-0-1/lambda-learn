f0419b36413373e8db7f9d375a59057d8f9fc97c
anjanasilva99 <akirindasilva@gmail.com>
2023-04-20 16:16:19 +0530

    Added two post requests to create submissions and update visibility.

--- public/index.php
+++ public/index.php
@@ -41,7 +41,8 @@ $app->router->get('/utilization', [SummaryViewController::class, 'displayUtiliza
 
 $app->router->get('/submissions', [CourseController::class, 'displayAllSubmissions']);
 $app->router->get('/marks_upload', [CourseController::class, 'displayCourseMarkUpload']);
-$app->router->post('/upload-submission', [ProfileController::class, 'CreateSubmission']);
+$app->router->post('/submissions', [CourseController::class, 'CreateSubmission']);
+$app->router->post('/submission_visibility', [CourseController::class, 'SubmissionVisibility']);
 
 
 $app->router->get('/leaderboard', [LeaderboardController::class, 'displayLeaderboard']);
