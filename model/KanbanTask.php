eb0c9dffce1121f39911a0ed83a50a73d432bd10
Dilanga-Harshani <aadilangaharshani@gmail.com>
2023-04-20 21:00:49 +0530

    Selection based on user type removed, lec and stu reg_no merged into one

--- model/KanbanTask.php
+++ model/KanbanTask.php
@@ -7,26 +7,22 @@ use app\core\User;
 
 class KanbanTask
 {
-
     private string $taskId;
     private string $title;
     private string $description;
     private string $dueDate;
     private string $state;
-    private string $stu_reg_no;
-    private string $lec_reg_no;
+    private string $regNo;
 
     private function __construct() {}
 
-    public static function createNewKanbanTask($title, $description, $state, $dueDate='',
-                                               $stu_reg_no='', $lec_reg_no='', $taskId=''): KanbanTask {
+    public static function createNewKanbanTask($title, $description, $state, $regNo, $dueDate='', $taskId=''): KanbanTask {
         $task = new KanbanTask();
         $task->title = $title;
         $task->description = $description;
         $task->state = $state;
         $task->dueDate = $dueDate;
-        $task->stu_reg_no = $stu_reg_no;
-        $task->lec_reg_no = $lec_reg_no;
+        $task->regNo = $regNo;
         $task->taskId = $taskId;
 
         return $task;
@@ -35,27 +31,18 @@ class KanbanTask
     public static function getToDoTasks(User $user): array
     {
         $kanbanTasks = [];
-        $type = $user::getUserType($user->getRegNo());
-        if ($type == 'Student') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['stu_reg_no' => $user->getRegNo(), 'state' => 'To Do'],
-                order: 'due_date ASC'
-            );
-        } elseif ($type == 'Lecturer') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['lec_reg_no' => $user->getRegNo(), 'state' => 'To Do'],
-                order: 'due_date ASC'
-            );
-        }
+        $results = Application::$db->select(
+            table: 'KanbanTask',
+            columns: ['task_id', 'title', 'description', 'due_date', 'state'],
+            where: ['reg_no' => $user->getRegNo(), 'state' => 'To Do'],
+            order: 'due_date ASC'
+        );
         while ($kanbanTask = Application::$db->fetch($results)) {
             $kanbanTasks[] = self::createNewKanbanTask(
                 title: $kanbanTask['title'],
                 description: $kanbanTask['description'],
                 state: $kanbanTask['state'],
+                regNo: $user->getRegNo(),
                 dueDate: $kanbanTask['due_date'],
                 taskId: $kanbanTask['task_id']
             );
@@ -66,27 +53,18 @@ class KanbanTask
     public static function getInProgressTasks(User $user): array
     {
         $kanbanTasks = [];
-        $type = $user::getUserType($user->getRegNo());
-        if ($type == 'Student') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['stu_reg_no' => $user->getRegNo(), 'state' => 'In Progress'],
-                order: 'due_date ASC'
-            );
-        } elseif ($type == 'Lecturer') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['lec_reg_no' => $user->getRegNo(), 'state' => 'In Progress'],
-                order: 'due_date ASC'
-            );
-        }
+        $results = Application::$db->select(
+            table: 'KanbanTask',
+            columns: ['task_id', 'title', 'description', 'due_date', 'state'],
+            where: ['reg_no' => $user->getRegNo(), 'state' => 'In Progress'],
+            order: 'due_date ASC'
+        );
         while ($kanbanTask = Application::$db->fetch($results)) {
             $kanbanTasks[] = self::createNewKanbanTask(
                 title: $kanbanTask['title'],
                 description: $kanbanTask['description'],
                 state: $kanbanTask['state'],
+                regNo: $user->getRegNo(),
                 dueDate: $kanbanTask['due_date'],
                 taskId: $kanbanTask['task_id']
             );
@@ -97,27 +75,18 @@ class KanbanTask
     public static function getDoneTasks(User $user): array
     {
         $kanbanTasks = [];
-        $type = $user::getUserType($user->getRegNo());
-        if ($type == 'Student') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['stu_reg_no' => $user->getRegNo(), 'state' => 'Done'],
-                order: 'due_date DESC'
-            );
-        } elseif ($type == 'Lecturer') {
-            $results = Application::$db->select(
-                table: 'KanbanTask',
-                columns: ['task_id', 'title', 'description', 'due_date', 'state'],
-                where: ['lec_reg_no' => $user->getRegNo(), 'state' => 'Done'],
-                order: 'due_date DESC'
-            );
-        }
+        $results = Application::$db->select(
+            table: 'KanbanTask',
+            columns: ['task_id', 'title', 'description', 'due_date', 'state'],
+            where: ['reg_no' => $user->getRegNo(), 'state' => 'Done'],
+            order: 'due_date DESC'
+        );
         while ($kanbanTask = Application::$db->fetch($results)) {
             $kanbanTasks[] = self::createNewKanbanTask(
                 title: $kanbanTask['title'],
                 description: $kanbanTask['description'],
                 state: $kanbanTask['state'],
+                regNo: $user->getRegNo(),
                 dueDate: $kanbanTask['due_date'],
                 taskId: $kanbanTask['task_id']
             );
@@ -134,15 +103,13 @@ class KanbanTask
                 'description' => $this->description,
                 'state' => $this->state,
                 'due_date' => $this->dueDate,
-                'stu_reg_no' => $this->stu_reg_no ?? '',
-                'lec_reg_no' => $this->lec_reg_no ?? ''
+                'reg_no' => $this->regNo
             ]
         );
     }
 
     public static function deleteKanbanTask($taskIdDlt)
     {
-        var_dump($taskIdDlt);
         Application::$db->delete(
             table: 'KanbanTask',
             where: ['task_id' => $taskIdDlt]
