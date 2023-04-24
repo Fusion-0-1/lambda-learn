c27cf476922cdf228bcd8c5adec56ac59b8b6d73
anjanasilva99 <akirindasilva@gmail.com>
2023-04-24 15:16:09 +0530

    Deleted profile images

--- views/dashboard.php
+++ views/dashboard.php
@@ -5,22 +5,20 @@
         <h3>Dashboard</h3>
         <?php
         try {
-            if (unserialize($_SESSION['user'])->isCoordinator()) {
-                $_SESSION['user-role'] = $_GET['user-role'] ?? 'Coordinator';
-                ?>
-                <form class="selector flex flex-row v-center" method="get" action="/">
-                    <p>Account : </p>
-                    <select onchange="this.form.submit()" name="user-role">
-                        <option value="Lecturer">Lecturer</option>
-                        <option value="Coordinator" <?php if($_SESSION['user-role'] == 'Coordinator') echo "selected"?>>
-                            Coordinator
-                        </option>
-                    </select>
-                </form>
-            <?php }} catch (\Throwable $th) {} ?>
+        if (unserialize($_SESSION['user'])->isCoordinator()) {
+            $_SESSION['user-role'] = $_GET['user-role'] ?? 'Coordinator';
+            ?>
+            <form class="selector flex flex-row v-center" method="get" action="/">
+                <p>Account : </p>
+                <select onchange="this.form.submit()" name="user-role">
+                    <option value="Lecturer">Lecturer</option>
+                    <option value="Coordinator" <?php if($_SESSION['user-role'] == 'Coordinator') echo "selected"?>>
+                        Coordinator
+                    </option>
+                </select>
+            </form>
+        <?php }} catch (\Throwable $th) {} ?>
     </div>
-
-    <h3>Dashboard</h3>
     <div class="card">
         <!-- Common cards -->
         <a href="/calender" class="link">
