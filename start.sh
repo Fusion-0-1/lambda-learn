55524f085cd9e82a6934f7f93973ae93833e9534
Ramindu Walgama <raminduw200@gmail.com>
2023-04-28 23:46:51 +0530

    Document the code

--- start.sh
+++ start.sh
@@ -20,10 +20,16 @@ fi
 
 #--------------------performance script-------------------
 echo "Starting performance tracking script. Logs are in scripts/logs/performance.log"
-nohup scripts/./performance_tracking.sh $params>> scripts/logs/performance.log &
+nohup scripts/./performance_tracking.sh $params >> scripts/logs/performance.log &
 
 #----------------------server starts----------------------
 cd public/
+# sed: the command to invoke the sed utility
+# -i: tells sed to edit the file in place
+# -e: specifies that the next parameter is a script to be executed
+# "16s@\$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env');@\$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env.test');@":
+# the script to be executed. In this case, it tells sed to substitute the string "$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env');", which is found on line 16 of the index.php file, with "$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env.test');".
+# index.php: the file to be edited.
 if [ "$env" == "test" ]; then
   sed -i -e "16s@\$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env');@\$dotenv = Dotenv::createImmutable(dirname(__DIR__), '.env.test');@" index.php
   nohup php -S "$HOST_TEST":"$PORT_TEST" >> ../scripts/logs/server.log &
