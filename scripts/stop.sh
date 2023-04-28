ee410a9fa34abad31fdbd0b18ce5d5c016758122
Ramindu Walgama <raminduw200@gmail.com>
2023-04-28 23:15:00 +0530

    Add stop performance script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,9 +2,14 @@
 # Path: scripts/stop.sh
 . scripts/read_env_variables.sh
 
+PID_PERFORMANCE=$(ps aux | grep "performance_tracking.sh" |  grep -v grep | awk '{print $2}')
 PID_PHP=$(ps aux | grep "php -S $HOST:$PORT" | grep -v grep | awk '{print $2}')
 PID_PHP_TEST=$(ps aux | grep "php -S $HOST_TEST:$PORT_TEST" | grep -v grep | awk '{print $2}')
 
+if [ "$PID_PERFORMANCE" != "" ]; then
+  echo "Stopping performance_tracking.sh running on $PID_PERFORMANCE"
+  kill "$PID_PERFORMANCE"
+fi
 if [ "$PID_PHP" != "" ]; then
   echo "Stopping PHP server on $HOST:$PORT"
   kill "$PID_PHP"
